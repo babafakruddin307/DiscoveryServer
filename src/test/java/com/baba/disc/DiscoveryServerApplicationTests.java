89d22f5b9873592006f78b17604736ead28d4572
Baba Fakruddin <babafakruddin307@gmail.com>
2023-01-09 16:03:18 +0530

    first

---
+++ src/test/java/com/baba/disc/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baba.disc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
